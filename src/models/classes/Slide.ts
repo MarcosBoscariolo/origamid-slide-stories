e341665979b41496d3718152b02933a51664c06b
MarcosBoscariolo <marcosboscariolo@hotmail.com>
2023-01-04 18:01:34 -0300

    feat: show slide

--- src/models/classes/Slide.ts
+++ src/models/classes/Slide.ts
@@ -1,8 +1,10 @@
 export default class Slide {
-    container;
-    slides;
-    controls;
-    timer;
+  container: Element;
+  slides: Element[];
+  controls: Element;
+  timer: number;
+  index: number;
+  slide: Element;
   constructor(
     container: Element,
     slides: Element[],
@@ -13,5 +15,20 @@ export default class Slide {
     this.slides = slides;
     this.controls = controls;
     this.timer = timer;
+    this.index = 0;
+    this.slide = this.slides[this.index];
+
+    this.showSlide(this.index);
+  }
+
+  hide(element: Element) {
+    element.classList.remove("active");
+  }
+
+  showSlide(index: number) {
+    this.index = index;
+    this.slide = this.slides[this.index];
+    this.slides.forEach((slide) => this.hide(slide));
+    this.slides[index].classList.add("active");
   }
 }
--- src/script.ts
+++ src/script.ts
@@ -1,9 +1,15 @@
 import Slide from "./models/classes/Slide.js";
 
-const container = document.getElementById('slide');
-const slides = document.getElementById('slide-elements');
-const control = document.getElementById('slide-controls');
+const container = document.getElementById("slide");
+const slides = document.getElementById("slide-elements");
+const control = document.getElementById("slide-controls");
 
-if(container && slides && control && slides.children.length) {
-    new Slide(container, Array.from(slides.children), control, 3000);
+if (container && slides && control && slides.children.length) {
+  const slide = new Slide(
+    container,
+    Array.from(slides.children),
+    control,
+    3000
+  );
+  slide.showSlide(2);
 }
